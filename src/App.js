fd7acebe04bbe892cd969761696d0fe44261f479
rishav7549 <rsr84068@gmail.com>
2023-05-27 21:27:02 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+// import "./App.css";
+
+// import { Pages } from "@mui/icons-material";
+// import { Home } from "react-feather";
+import Sidebar from "./Sidebar";
+// import TodoBody from "./components/TodoBody";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Sidebar />
+      {/* <TodoBody /> */}
+      {/* <Home /> */}
+    </>
   );
 }
 
